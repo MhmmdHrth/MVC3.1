d8b5e46b037dd269b6f7960601f3422d2d3f1222
Muhammad Harith <harith781@gmail.com>
2020-11-16 15:25:41 +0800

    seed data

---
+++ EmployeeManagement/Migrations/20201116072510_addSeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class addSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 1, 2, "harith@gmail.com", "Harith" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "ahmad@gmail.com", "Ahmad" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/ModelBuilderExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                        .HasData(
+                                    new Employee
+                                    {
+                                        Id = 1,
+                                        Department = DepartmentType.IT,
+                                        Email = "harith@gmail.com",
+                                        Name = "Harith"
+                                    },
+                                    new Employee
+                                    {
+                                        Id = 2,
+                                        Department = DepartmentType.HR,
+                                        Email = "ahmad@gmail.com",
+                                        Name = "Ahmad"
+                                    }
+                                );
+        }
+    }
+}
