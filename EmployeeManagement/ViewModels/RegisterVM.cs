12b33d7f0623acb36f446995a1cb9aa1205638c2
Muhammad Harith <harith781@gmail.com>
2021-01-30 09:11:36 +0800

    register user, include UserManager<> & SignInManager<>

---
+++ EmployeeManagement/ViewModels/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirmation password do not match!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
