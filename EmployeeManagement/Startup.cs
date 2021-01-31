5115633326abb1d86a4aaf67dd195b5faafff839
Muhammad Harith <harith781@gmail.com>
2021-02-01 01:54:55 +0800

    globally authorize action

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -4,10 +4,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,7 +41,16 @@ namespace EmployeeManagement
 
             
 
-            services.AddMvc(config => config.EnableEndpointRouting = false);
+            services.AddMvc(config => {
+                config.EnableEndpointRouting = false;
+
+                var policy = new AuthorizationPolicyBuilder()
+                                .RequireAuthenticatedUser()
+                                .Build();
+
+                config.Filters.Add(new AuthorizeFilter(policy));
+
+            });
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
         }
 
@@ -57,9 +68,9 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
 
-            app.UseRouting();
-
             app.UseAuthentication();
+
+            app.UseRouting();
 
             app.UseMvc(route =>
             {
