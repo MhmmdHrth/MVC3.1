49e2fdcbad9f021ae2b3c26cd40e2fe683823720
Muhammad Harith <harith781@gmail.com>
2021-02-09 08:19:02 +0800

    all about role (list, update, add)

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagement.ViewModels;
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,13 @@ namespace EmployeeManagement.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -52,6 +56,58 @@ namespace EmployeeManagement.Controllers
         public IActionResult ListRoles()
         {
             return View(roleManager.Roles);
+        } 
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+           var role = await roleManager.FindByIdAsync(id);
+
+           if(role != null)
+            {
+                var model = new EditRoleVM
+                {
+                    Id = role.Id,
+                    RoleName = role.Name
+                };
+
+               foreach(var user in await userManager.GetUsersInRoleAsync(role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+
+                return View(model);
+            }
+
+            ViewBag.ErrorMessage = $"Role with ID:{id} cannot be found";
+            return View("NotFound");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleVM model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role != null)
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+
+            ViewBag.ErrorMessage = $"Role with ID:{model.Id} cannot be found";
+            return View("NotFound");
         }
     }
 }
--- EmployeeManagement/ViewModels/CreateRoleVM.cs
+++ EmployeeManagement/ViewModels/CreateRoleVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace EmployeeManagement.ViewModels
 {
     public class CreateRoleVM
     {
+        [Required]
         public string RoleName { get; set; }
     }
 }
